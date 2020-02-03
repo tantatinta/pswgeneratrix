e87f9d4eeedb1c431cba6cd0389c1d92a99eb407
tantatinta <bluekasia@gmail.com>
2020-02-02 20:28:49 -0500

    updating file structure and adding read me

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pswgeneratrix
-Password generator that allows to pick sets of characters. First attempt.
+Password generator that allows to pick sets of characters. 
+
+Upon clicking "Generate", user is presented with a series of prompts for password criteria
+including lower and uppercase letters, special characters and numbers. User is aldo able to select the length of their password from a range of 8 to 128 characters.
+
+```
+
+The following images show the application functionality:
+
+![password generator demo](./assets/first-alert.png)
+![password generator demo](./assets/number-prompt.png)
+
---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+var specialChar = ["!", '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', "~"];
+var alphaLower = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphaUpper = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = [0, 1, 2, 3, 4, 5, 6 , 7, 8, 9];
+
+generateBtn.addEventListener("click", generatePsw);
+
+function generatePsw(x) {
+  alert("Hello! Please read carefully to select your password specifications.");  
+  var varHolder;
+  var wantsSpecialChar = confirm("Click 'OK' for 'YES' and 'Cancel' for 'NO'. Would you like to include special characters--e.g., '%', '$', or '('-- in your password?");
+  var wantsLowerCase = confirm("Would you like to include lowercase characters 'a' to 'z' in your password?");
+  var wantsUpperCase = confirm("Would you like to include uppercase characters 'A' to 'Z' in your password?");
+  var wantsNumbers = confirm("Would you like to include numbers (0 to 9) in your password?");
+  var preferredLength = parseInt(prompt("Please write your choice of character length in numbers from 8 to 128."));
+    while (preferredLength < 8 || preferredLength > 128)
+    var preferredLength = parseInt(prompt("Please write your choice of character length in numbers from 8 to 128."));   
+        
+        if (wantsSpecialChar === true) {
+          varHolder += [specialChar];
+        }
+
+        if (wantsLowerCase === true) {
+          varHolder += [alphaLower];
+        }
+
+        if (wantsLowerCase === true) {
+          varHolder += [alphaUpper];
+        }
+
+        if (wantsNumbers === true) {
+          varHolder += [numbers];
+        }
+
+        if (varHolder === undefined) {
+          alert("You must select at least one character category.");
+          generatePsw();
+        }
+
+
+  for (k = 0; k < preferredLength; k++) {
+    var randomItem = varHolder[Math.floor(Math.random() * varHolder.length)];    
+          
+    alert(randomItem);   
+  }
+  writePassword();
+  }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
